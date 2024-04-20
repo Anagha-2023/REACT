d4343a9b6db03fc292c2cc476e88a8632a4afcc5
Anagha-2023 <140034415+Anagha-2023@users.noreply.github.com>
2024-04-20 18:52:59 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css'
-import Header from './components/Header'
+import { useState } from "react";
 
-function App(){
-  const data = "React learner"
+function App() {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <Header data={data}/>
+      <button onClick={() => setCount(count + 1)}>Incremet</button>
+      <button onClick={() => setCount(count - 1)} disabled={count === 0}>Decrement</button>
+      <button onClick={() => setCount(0)} disabled={count === 0}>Reset</button>
+      <h1>Counter: {count}</h1>
     </div>
-    
-  )
+  );
 }
-export default App;
 
+export default App;
