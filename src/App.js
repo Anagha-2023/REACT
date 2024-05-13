e4f24b2e65dd52381da6351976a093c6d66e3d58
Anagha-2023 <140034415+Anagha-2023@users.noreply.github.com>
2024-05-13 12:30:07 +0530

    fetch data using axios and display

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import axios from 'axios'
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const emp = ["Jack", "Rose"];
-
+  const [state,setState] = useState([])
   return (
     <div>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      {count > 0 && <p>{emp[count-1]}</p>}
+      <h1>Hello World</h1>
+      <button onClick={()=>{
+        axios.get('https://jsonplaceholder.typicode.com/posts').then((response)=>{
+          console.log(response.data);
+          setState(response.data)
+        })
+      }}>Click Me</button>
+      {state.map((obj,index)=>{
+        return(
+          <div>
+            <h1>{index+1}</h1>
+            <h1>{obj.title}</h1>
+            <h4>{obj.body}</h4>
+          </div>
+        )
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
